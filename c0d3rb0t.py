02d844c93f638de7e10597d9069f637d36987b2a
TylerJaacks <TylerJaacks@Gmail.com>
2017-06-08 00:22:27 -0500

    Finshed the graphing command.

--- c0d3rb0t.py
+++ c0d3rb0t.py
@@ -7,19 +7,33 @@ import sympy as sympify
 
 from discord.ext.commands import Bot
 from datetime import datetime
+from math import sin, cos, tan
 
 def create_plot(expression):
-    fileName = datetime.now().strftime('%H%M%S') + ".png"
+    fileName = "graphs/" + datetime.now().strftime('%H%M%S') + ".png"
 
-    x = np.array(range(0, 10))
+    x = np.array(range(-50, 50))
     y = eval(expression)
 
     fig = plt.figure(1)
-    plt.plot(x, y)
 
-    plt.savefig("graphs\" + fileName)
+    fig.suptitle("The graph of " + expression, fontsize=20)
+
+    plt.xlabel('X', fontsize=18)
+    plt.ylabel('Y', fontsize=18)
+
+    plt.plot(x, y)
+    plt.savefig(fileName)
+    plt.clf()
 
     return fileName
+
+def str_to_int(s):
+    ctr = i = 0
+    for c in reversed(s):
+        i += (ord(c) - 48) * (10 ** ctr)
+        ctr += 1
+    return i
 
 client = discord.Client()
 
@@ -37,5 +51,10 @@ async def on_message(message):
 
         with open(fileName, 'rb') as f:
             await client.send_file(message.channel, fileName)
+
+    if message.content.startswith('!eval'):
+        expression = message.content[5:len(message.content)]
+
+        await client.send_message(message.channel, eval(expression))
 
 client.run('MjczODc1NDcwMDM3MTU1ODQx.DBoTzQ.q5XaH1YAeSVnREqitLK2Qsqneoo')
