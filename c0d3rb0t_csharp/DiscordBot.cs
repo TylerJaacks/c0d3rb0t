913d5c26bfcb4c8c3d331ee3260b5fa65b6844d2
TylerJaacks <TylerJaacks@Gmail.com>
2017-07-16 18:54:37 -0500

    Added some stuff.
    Can't remeber what though.

---
+++ c0d3rb0t_csharp/DiscordBot.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+
+namespace c0d3rb0t_csharp
+{
+    public class DiscordBot
+    {
+        public DiscordBot()
+        {
+            DiscordClient client;
+        }
+    }
+}
---
+++ c0d3rb0t_csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c0d3rb0t_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscordBot bot = new DiscordBot();
+        }
+    }
+}
