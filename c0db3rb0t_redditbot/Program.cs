a91bfed83cfe3250926641c8924cedbc52973bb0
Tyler Jaacks <tylerjaacks@gmail.com>
2017-08-16 03:27:10 -0500

    Added Reddit bot.

---
+++ c0db3rb0t_redditbot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c0db3rb0t_redditbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedditBot redditBot = new RedditBot(args[0], args[1]);
+        }
+    }
+}
---
+++ c0db3rb0t_redditbot/RedditBot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RedditSharp;
+using RedditSharp.Things;
+
+namespace c0db3rb0t_redditbot
+{
+    class RedditBot
+    {
+        public RedditBot(String username, String password)
+        {
+            BotWebAgent userAgent = new BotWebAgent(username, password, "zC2ev-zjX8P_5A", "CazRtoCuSIjY5829OV7H9NwO4O8", "http://localhost:8080");
+            Reddit reddit = new Reddit(userAgent, false);
+            Subreddit subreddit = reddit.GetSubreddit("vitalc0d3r");
+        }
+    }
+}
