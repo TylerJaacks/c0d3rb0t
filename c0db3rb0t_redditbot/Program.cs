972188b956f10c2cf61cebdc082ff2c19fcaa5aa
Tyler Jaacks <tylerjaacks@gmail.com>
2017-09-22 19:23:34 -0500

    All the stuff.

--- c0db3rb0t_redditbot/Program.cs
+++ c0db3rb0t_redditbot/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace c0db3rb0t_redditbot
+﻿using System;
+using RedditSharp;
+
+namespace c0db3rb0t_redditbot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            RedditBot redditBot = new RedditBot(args[0], args[1]);
+            RedditBot redditBot;
+
+            try
+            {
+                redditBot = new RedditBot(args[0]);
+            }
+
+            catch (Exception exception)
+            {
+                Console.WriteLine("An error has occured, try again! \n" + exception.Data + "\n" + exception.StackTrace + "\n");
+
+                System.Environment.Exit(1);
+            }
         }
     }
 }
--- c0db3rb0t_redditbot/RedditBot.cs
+++ c0db3rb0t_redditbot/RedditBot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using RedditSharp;
 using RedditSharp.Things;
 
@@ -6,11 +7,16 @@ namespace c0db3rb0t_redditbot
 {
     class RedditBot
     {
-        public RedditBot(String username, String password)
+        public RedditBot(String password)
         {
-            BotWebAgent userAgent = new BotWebAgent(username, password, "zC2ev-zjX8P_5A", "CazRtoCuSIjY5829OV7H9NwO4O8", "http://localhost:8080");
+            BotWebAgent userAgent = new BotWebAgent("vitalc0d3r", password, "zC2ev-zjX8P_5A", "CazRtoCuSIjY5829OV7H9NwO4O8", "http://localhost:8080");
             Reddit reddit = new Reddit(userAgent, false);
-            Subreddit subreddit = reddit.GetSubreddit("vitalc0d3r");
+            Subreddit subreddit = reddit.GetSubreddit("pcmasterrace");
+
+            foreach (Post post in subreddit.New.Take(100))
+            {
+                Console.WriteLine(post.Title + "\n" + post + "\n");
+            }
         }
     }
 }
